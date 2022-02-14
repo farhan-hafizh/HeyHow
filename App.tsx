99329006173ae2e9255a27446546865170d529f4
Farhan Novyan Hafizh <64812051+farhan-hafizh@users.noreply.github.com>
2022-02-14 11:30:59 +0700

    AWS Auth Setup

--- App.tsx
+++ App.tsx
@@ -5,11 +5,14 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import { withAuthenticator} from 'aws-amplify-react-native'
+
+
 import Amplify from 'aws-amplify'
 import awsconfig from './src/aws-exports'
 Amplify.configure(awsconfig)
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -24,3 +27,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App); //aws auth
